1cbc2c151a8d91655341a83c984b14c77a862dec
Akash Gupta <akash.gupta@buopso.com>
2024-03-19 15:46:56 +0530

    changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,11 @@ import React from "react";
 function Header() {
   return (
     <div className="uppercase text-center border-b-2 font-bold p-2">
+<<<<<<< HEAD
       Suresh blogs
+=======
+      Aman blogs
+>>>>>>> 91e984383c547f2e82ed936b14862fd5490c1aab
     </div>
   );
 }
